e88b4a7caea2863a1f88274a94d62cea589321ea
saisrivathsa <srivathsaeric@gmail.com>
2017-02-04 01:56:55 +0530

    ADDED LOGIN AND NEW USER FORMS

---
+++ auth_model/views.py
@@ -0,0 +1,48 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+#for authentication uses
+from django.contrib import auth
+# for new user
+from django.contrib.auth.models import User
+
+def logout(request) :
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('auth_model:login'))
+
+def success(request) :
+    return render(request, 'auth_model/success.html', {'username' : request.user.username})
+
+#gets values from the form and checks whether user exists
+def authenticate_method(request) :
+    username = request.POST.get('username', '' )
+    password = request.POST.get('password', '' )
+    user = auth.authenticate( username=username, password=password )
+    # The default value when a user is not found is none
+    if user is not None :
+        auth.login( request, user)
+        return HttpResponseRedirect(reverse("auth_model:success"))
+    else :
+        return render(request, "auth_model/login.html", {'error':'Invalid Credentials'})
+        # Takes back to login page with an error message
+
+# Creates a new user by taking name, mail, mobile number and password
+def create_new_user(request) :
+    try:
+        user = User.objects.create_user(
+        username = request.POST['username'],
+        password = request.POST['password'],
+        email = request.POST['email'],
+        )
+        # Default fields for user class in django doesn't contain certain fields which can be added using extra_fields
+        user.extra_fields = {'phone_number' : request.POST['phone_number']}
+        # Saving the user data
+        user.save()
+        username = request.POST.get('username', '' )
+        password = request.POST.get('password', '' )
+        # Logging in the user after creating the new user
+        user = auth.authenticate( username=username, password=password )
+        return HttpResponse('<h2>Created new user %s </h2><br><a href="../sign_up/">  create another account?</a>' %user.username)
+    except :
+        return render( request, 'auth_model/sign_up.html', {'error':'Error making account. Try again'} )
+        # Returning to signup page in case of error in making the account
--- gregary/urls.py
+++ gregary/urls.py
@@ -5,6 +5,7 @@ app_name = 'gregary'
 
 urlpatterns = [
 url(r'^$', views.index, name='index'),
+url(r'^auth/', include('auth_model.urls')),
 url(r'^register_event/$', views.register_event, name='register_event'),
 url(r'^event_create/$', views.event_create, name='event_create'),
 url(r'^events/$', views.coming_events, name='coming_events'),
