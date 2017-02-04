a9a3ae37302caa577e663d1788ac4cf29573bc32
saisrivathsa <srivathsaeric@gmail.com>
2017-02-05 02:43:16 +0530

    MODIFICATIONS TO EVENT REGISTRATION

---
+++ gregary/static/gregary/js/subchanger.js
@@ -0,0 +1,25 @@
+(function(){
+  var sub_categoryOptions = {"food":["Pizza","Paneer","Coming soon"], "computer_games":["Fifa","CS","COD"], "games":["Cricket","Volleyball","Chess"]};
+  var category = document.getElementById('category');
+  var sub_category = document.getElementById('sub_category');
+  //on change is a good event for this because you are guarenteed the value is different
+  category.onchange = function(){
+      //clear out sub_category
+      sub_category.length = 0;
+      //get the selected value from category
+      var _val = this.options[this.selectedIndex].value;
+      //loop through sub_categoryOption at the selected value
+      for ( var i in sub_categoryOptions[_val]){
+          //create option tag
+          var op = document.createElement('option');
+          //set its value
+          op.value = sub_categoryOptions[_val][i].toLowerCase();
+          //set the display label
+          op.text = sub_categoryOptions[_val][i];
+          //append it to sub_category
+          sub_category.appendChild(op);
+      }
+  };
+  //fire this to update sub_category on load
+  category.onchange();
+})();
--- gregary/views.py
+++ gregary/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect, HttpResponse
 from django.urls import reverse
-from .models import Sport
+from .models import Event
 from django.utils import timezone
 '''
 from django.views import generic
@@ -9,17 +9,39 @@ from django.db.models import F
 '''
 
 def coming_events(request):
-    latest_event_list = Sport.objects.order_by('start_time')
+    latest_event_list = Event.objects.order_by('start_time')
     context = {'latest_event_list': latest_event_list}
     return render(request, 'gregary/coming_events.html', context)
 
 def event_create(request):
+    if ( len(request.POST['enrollment_number']) != 8 ):
+        return render(request, 'gregary/register_event.html', {'error': 'Invalid enrollment number'})
+    if ( len(request.POST['phone_number']) != 10 ):
+        return render(request, 'gregary/register_event.html', {'error': 'Invalid mobile number'})
+    new_event = Event(
+        category = request.POST['category'],
+        sub_category = request.POST['sub_category'],
+        student = request.POST['student'],
+        enrollment_number = request.POST['enrollment_number'],
+        phone_number = request.POST['phone_number'],
+        start_time = request.POST['start_time'],
+        end_time = request.POST['end_time']
+    )
+    if(new_event.start_time >= new_event.end_time):
+        return render(request, 'gregary/register_event.html', {'error': 'Start time cannot be after end time'})
+    new_event.save()
+    return HttpResponseRedirect(reverse('gregary:coming_events'))
+
+'''
+    #if(new_event.end_time <= timezone.now()):
+    #    return render(request, 'gregary/register_event.html', {'error' : 'Event can\'t end in the past'})
     try :
         if ( len(request.POST['enrollment_number']) != 8 ):
             return render(request, 'gregary/register_event.html', {'error': 'Invalid enrollment number'})
         if ( len(request.POST['phone_number']) != 10 ):
             return render(request, 'gregary/register_event.html', {'error': 'Invalid mobile number'})
-        new_event = Sport(
+        new_event = Event(
+            category = request.POST['category'],
             sub_category = request.POST['sub_category'],
             student = request.POST['student'],
             enrollment_number = request.POST['enrollment_number'],
@@ -37,3 +59,4 @@ def event_create(request):
 
     except:
         return render(request, 'gregary/register_event.html', {'error': 'Fill all the required fields'})
+'''
