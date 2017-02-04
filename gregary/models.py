90bee9b7d5daf7a9ae87e5d6208a3062117198d2
saisrivathsa <srivathsaeric@gmail.com>
2017-02-05 02:43:16 +0530

    MODIFICATIONS TO EVENT REGISTRATION

--- gregary/models.py
+++ gregary/models.py
@@ -1,7 +1,8 @@
 from __future__ import unicode_literals
 from django.db import models
 
-class Sport( models.Model ):
+class Event( models.Model ):
+    category = models.TextField( default=0, max_length=50, blank=False )
     sub_category = models.TextField( default=0, max_length=50, blank=False )
     start_time = models.DateTimeField( default=0, blank=False )
     end_time = models.DateTimeField( default=0, blank=False )
