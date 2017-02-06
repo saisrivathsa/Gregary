665ec38ed149a441ff3526dac8634e06d0704d83
Vinam Arora <vinam.arora8@gmail.com>
2017-02-06 18:01:39 +0530

    Adds samples for Header and Homepage

--- gregary/static/gregary/samples/titlebar.js
+++ gregary/static/gregary/samples/titlebar.js
@@ -1,4 +1,12 @@
+function isLoggedIn() {
+    'use strict';
+    
+    return false;
+}
+
 function drawLoginButton() {
+    // This function will create the login-button into HTML
+    'use strict';
     // Create login-button
     var login_button = document.createElement('button');
     login_button.className = 'custom-button';
@@ -14,14 +22,54 @@ function drawLoginButton() {
     document.getElementById('uinfo-outer-container').appendChild(login_button);
 }
 
-//document.onload += drawLoginButton();
-/*
-document.onload += function() {
-    if (isLoggedIn) {
+function drawUserInfo() {
+    // This function will create the user-info into HTML
+    'use strict';
+    ////Image
+    
+    // Create image container
+    var profile_picture = document.createElement('img');
+    profile_picture.id = 'profile-picture';
+    profile_picture.src = 'dp.jpg';     // Change this dynamically, depending on the current user
+    
+    // Put the image in the uinfo-outer-container
+    document.getElementById('uinfo-outer-container').appendChild(profile_picture);
+    
+    //// Uinfo
+    
+    // Create uinfo-container and text
+    var uinfo_container = document.createElement('div'),
+        space           = document.createElement('br'),
+        first_name_p    = document.createElement('p'),
+        last_name_p     = document.createElement('p'),
+        first_name_text = document.createTextNode('John'),      // Change these 2 dynamically
+        last_name_text  = document.createTextNode('von Neumann');
+
+    // Assign id and class names
+    uinfo_container.id      += 'uinfo-container';
+    space.className         += 'uinfo ';
+    first_name_p.className  += 'uinfo ';
+    last_name_p.className   += 'uinfo ';
+    first_name_p.className  += 'bold ';
+    
+    // Assign stuff to parents
+    first_name_p.appendChild(first_name_text);
+    last_name_p.appendChild(last_name_text);
+    
+    uinfo_container.appendChild(space);
+    uinfo_container.appendChild(first_name_p);
+    uinfo_container.appendChild(last_name_p);
+    
+    // Put this container in the uinfo-outer-container
+    document.getElementById('uinfo-outer-container').appendChild(uinfo_container);
+}
+
+document.onload += (function () {
+    'use strict';
+    
+    if (isLoggedIn()) {
         drawUserInfo();
-    } else{
+    } else {
         drawLoginButton();
     }
-}
-*/
-document.onload += console.log("Loaded");
+}());
